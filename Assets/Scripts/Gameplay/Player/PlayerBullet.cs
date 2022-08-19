f31aa7f2c392a2e90fa2ffda131c9c8ce8ed9134
DESKTOP-VL8VUR3\A-23 <DESKTOP-VL8VUR3\A-23>
2022-08-19 11:10:40 +0800

    PowerUp

--- Assets/Scripts/Gameplay/Player/PlayerBullet.cs
+++ Assets/Scripts/Gameplay/Player/PlayerBullet.cs
@@ -10,12 +10,18 @@ namespace Aja.Gameplay
     {
         public GameObject enemyParent;
         public Text scoreText;
+        public bool Powered;
+
+        public GameObject Powerup;
 
         //int _score;
 
         void Start()
         {
             enemyParent = GameObject.Find("ENEMY");
+            Powerup = GameObject.Find("PU1");
+            Powered = false;
+            Invoke("PowerUpSpawn", 5.0f);
             //_score = 0;
         }
 
@@ -31,10 +37,35 @@ namespace Aja.Gameplay
             {
                 EnemyFire enemyfire = enemyParent.GetComponent<EnemyFire>();
                 enemyfire.swapAttack(collision.gameObject);
-                Destroy(gameObject);
-
+                if (Powered == true)
+                {
+                    Destroy(gameObject, 0.5f);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
                 //ScoreManager.PlayerScored();
             }
+
+            if (collision.gameObject.CompareTag("power"))
+            {
+                Powered = true;
+                Powerup.SetActive(false);
+
+                Invoke("PowerUpSpawn", 5.0f);
+                Invoke("PowerUpDespawn", 5.0f);
+            }
+        }
+
+        private void PowerUpDespawn()
+        {
+            Powered = false;
+        }
+
+        private void PowerUpSpawn()
+        {
+            Powerup.SetActive(true);
         }
     }
 }
